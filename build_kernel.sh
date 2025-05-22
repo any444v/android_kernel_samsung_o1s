0eab76b09ad434105bdad1b9191069bfb6b86d43
any444v <any444v@proton.me>
2025-05-23 03:55:59 +0500

    Multi-thread support

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,6 @@ export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=15
 
-make ARCH=arm64 exynos2100-o1sxxx_defconfig
-make ARCH=arm64 -j16
+make clean -j$(nproc)
+make exynos2100-o1sxxx_defconfig -j$(nproc)
+make -j$(nproc)
